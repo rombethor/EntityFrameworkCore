155e595686f331a00bf7c8618049d7311bd66bb7
Daniel Thorne <thorne.danielj@gmail.com>
2021-09-20 17:51:08 -0500

    Add project files.

---
+++ Rombethor.EntityFrameworkCore/EntityFrameworkCoreExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rombethor.EntityFrameworkCore
+{
+    public static class EntityFrameworkCoreExtensions
+    {
+        /// <summary>
+        /// Checks through all DbSets defined in the Data Context 'dbContext' and runs the method OnModelCreating for each model
+        /// pertaining to the interface IModelCreation
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <param name="modelBuilder"></param>
+        public static void UseSelfDefiningModels(this DbContext dbContext, ModelBuilder modelBuilder)
+        {
+            Type dbContextType = dbContext.GetType();
+            foreach (var prop in dbContextType.GetProperties())
+            {
+                var propType = prop.PropertyType;
+                if (propType.IsGenericType && propType.GetGenericTypeDefinition() == typeof(DbSet<>))
+                {
+                    var itemType = propType.GenericTypeArguments[0];//.GetGenericTypeDefinition().GetGenericArguments()[0];
+                    var method = itemType.GetMethod("OnModelCreating");
+                    if (typeof(IModelCreation).IsAssignableFrom(itemType) && method != null)
+                    {
+                        //If the "OnModelCreating(ModelBuilder modelBuilder)" method exists, run it with the given ModelBuilder
+                        method.Invoke(Activator.CreateInstance(itemType, false), new object[] { modelBuilder });
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Rombethor.EntityFrameworkCore/IModelCreation.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Rombethor.EntityFrameworkCore
+{
+    public interface IModelCreation
+    {
+        /// <summary>
+        /// For use with DbContext.UseSelfDefiningModels to define rules for the entity model within the class definition.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        /// <see cref="EntityFrameworkCoreExtensions.UseSelfDefiningModels(DbContext, ModelBuilder)"/>
+        public void OnModelCreating(ModelBuilder modelBuilder);
+    }
+}
